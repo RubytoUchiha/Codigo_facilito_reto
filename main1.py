f2f04951f7bfc5c8d269bfa5455d38a1636b10b6
RubytoUchiha <159063578+RubytoUchiha@users.noreply.github.com>
2024-02-05 19:28:54 +0100

    Reto1
    Reto 1 del codigofacilito

---
+++ main1.py
@@ -0,0 +1,8 @@
+###Reto dia 1
+
+nombre = input("Introduce tu nombre(s) ")
+apellido = input("Introduce tus apellidos ")
+telefono = input("Introduce tu nº de telefono ")
+correo = input("Introduce tu Correo electrónico ")
+
+print("Hola " + nombre + " " + apellido + ", en breve recibirás un correo a " + correo)
